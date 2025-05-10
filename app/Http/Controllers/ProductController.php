f27e25755f6d72824f529645276db6eab9c489e4
Chris Cantimbuhan <chriscantimbuhan@gmail.com>
2025-05-11 03:05:12 +0800

    Apply new attach tag methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -68,11 +68,12 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         $tags = array_merge(
-            $request->input('product_category', []),
             $request->input('product_experience', [])
         );
 
-        $product->syncTags($tags);
+        $product->syncTags($tags, $request);
+
+        $product->attachTag($request->input('product_category', null), 'product_category');
     }
 
     /**
